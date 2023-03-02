8031e4ddaf009a74d4c2d8e7be9c94ecddb12cae
Vivek Mahajan <vmahajan@hopper.com>
2023-03-02 07:19:58 +0100

    Buildable structure (#1)
    * Placeholder project structure
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Tony Morales <tmorales@hopper.com>

---
+++ HCHotels/Source/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  
+//
+//  Created by Tony Morales on 3/1/23.
+//
+
+import Foundation
+
+public class HCHotels {
+    public init() {
+        print("HCHotels instance created")
+    }
+    
+    public func helloWorld() {
+        print("Hello, World!")
+    }
+}
